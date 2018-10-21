13e1b7859bc47f27a7e2a136957155dc8a5e5285
DESKTOP-I486M1H\leoca <leocastanhob@gmail.com>
2018-10-21 14:30:51 -0300

    Player picking icons

--- Scripts/xNotepad.gd
+++ Scripts/xNotepad.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_xNotepad_body_entered(body):
 	if (body.name == "Player"):
-		Global.Player.pickup_key()
+		Global.Player.pickup_key("xnotepad")
 		get_node("..").queue_free()
--- xButton.gd
+++ xButton.gd
@@ -3,5 +3,5 @@ extends Area2D
 func _on_xButton_body_entered(body):
 	if Global.Player.key:
 		$ButtonBlock.queue_free()
-		Global.Player.destroy_key()
+		Global.Player.destroy_key("xnotepad")
 		$CloseButton.set_disabled(false)
